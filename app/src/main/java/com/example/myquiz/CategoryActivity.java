ff04c4a88734ec8a992bcbe78f8b5d1ca1ba39c2
mprzemo17 <mojescik.przemyslaw@gmail.com>
2021-01-02 09:31:14 +0100

    After three tutorials.

---
+++ app/src/main/java/com/example/myquiz/CategoryActivity.java
@@ -0,0 +1,49 @@
+package com.example.myquiz;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CategoryActivity extends AppCompatActivity {
+
+    private GridView catGrid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Categories");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        catGrid = findViewById(R.id.catGridView);
+
+        List<String> catList = new ArrayList<>();
+
+        catList.add("SATYSFAKCJA HAIR");
+        catList.add("BARBER CIESZYN"); //ilość kategorii
+
+        CatGridAdapter adapter = new CatGridAdapter(catList);
+        catGrid.setAdapter(adapter);
+
+   }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+        {
+            CategoryActivity.this.finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
